02f2f0ff257da7e869f47392bfda45c73050f7f7
Manuel Carrascal <tricdiart@gmail.com>
2024-11-19 11:36:00 -0500

    Sample operator

--- src/operadores-tiempo/03-sampleTime.ts
+++ src/operadores-tiempo/03-sampleTime.ts
@@ -4,8 +4,8 @@ const click$ = fromEvent<PointerEvent>(document, 'click');
 
 click$
 	.pipe(
-        sampleTime(2000), //es un poco mas eficiente que el sampleTime este arriba por tema de operaciones
-		map(({ x, y }) => ({ x, y })),
+		sampleTime(2000), //es un poco mas eficiente que el sampleTime este arriba por tema de operaciones
+		map(({ x, y }) => ({ x, y }))
 		//sampleTime(2000)
 	)
 	.subscribe(console.log);
